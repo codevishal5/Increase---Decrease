2c0d32d12cc1658e304d309f340d7ccef17276c0
codevishal5 <code.vishal5@gmail.com>
2025-03-25 22:19:23 +0530

    test

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-let count = 0;
-const countEl = document.getElementById("count");
-function increase(){
-    count++;
-    countEl.innerText = count;
-}
-function decrease(){
-    count--;
-    countEl.innerText = count;
-}
-function reset(){
-    count = 0;
-    countEl.innerText = count;
-}
+let count;
+
+    if(localStorage.getItem('count') === null) {
+        count = 0;
+    }
+    else {
+        count = localStorage.getItem('count');
+    }
+
+    document.querySelector('h2').textContent = count;
+
+    function increase() {
+        count++;
+        localStorage.setItem('count', count);
+        document.querySelector('h2').textContent = count;
+    }
+
+    function decrease() {
+        count--;
+        localStorage.setItem('count', count);
+        document.querySelector('h2').textContent = count;
+    }
+
+    function reset() {
+        count = 0;
+        localStorage.setItem('count', count);
+        document.querySelector('h2').textContent = count;
+    }
+
+    document.querySelector('.increase').addEventListener('click', increase);
+
+    document.querySelector('.decrease').addEventListener('click', decrease);
+
+    document.querySelector('.reset').addEventListener('click', reset);
+
